ecf088eb9f5849a12a4f20574d040063d0341342
henry <henryfan@msn.com>
2016-10-27 11:15:28 +0800

    no message

---
+++ BeetleX.Samples/BeetleX.BaseClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using BeetleX.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetleX.BaseClient
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            BeetleX.Clients.IClient client = ServerFactory.CreateTcpClient("127.0.0.1", 9090);
+            client.Receive = (c, e) =>
+            {
+                lock (e.SyncObject)
+                {
+
+                    Console.WriteLine(e.ReadLine());
+                }
+
+            };
+            client.Connect();
+            client.Writer.WriteLine(DateTime.Now.ToString());
+            client.Writer.Flush();
+
+            Console.Read();
+        }
+    }
+}
---
+++ BeetleX.Samples/BeetleX.BaseServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using BeetleX.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetleX.BaseServer
+{
+    public class Program : BeetleX.ServerHandlerBase
+    {
+        private static IServer server;
+        private static List<string> items = new List<string>();
+        public static void Main(string[] args)
+        {
+            NetConfig config = new NetConfig();
+            server = ServerFactory.CreateTcpServer<Program>(config);
+            server.Open();
+            Console.Read();
+        }
+        public override void Connected(IServer server, ConnectedEventArgs e)
+        {
+            base.Connected(server, e);
+            items.Clear();
+        }
+        public override void SessionReceive(IServer server, EventArgs.SessionReceiveEventArgs e)
+        {
+            string value = e.Reader.ReadLine();
+            Console.WriteLine(value);
+            e.Session.Writer.WriteLine(value);
+            e.Session.Writer.Flush();
+            base.SessionReceive(server, e);
+        }
+    }
+}
